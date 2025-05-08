db2a588098e3b0404f0be771e857fe7d5d38ba76
Sebastian Bonilla <sbonilla623@gmail.com>
2025-05-08 18:50:46 -0400

    index.js fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Optional styling
+
 console.log("Hello World");
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
