000fc0425f333bdf4989f7fa7a216b1f7a5db0fc
Sebastian Bonilla <sbonilla623@gmail.com>
2025-05-07 20:19:05 -0400

    file structure fix

--- models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Appointment = require('./Appointment');  // Note: singular
-const Professional = require('./Professional');
-
-// Change this line to use the correct variable name (Appointment, not Appointments)
-module.exports = { User, Appointment, Professional };
