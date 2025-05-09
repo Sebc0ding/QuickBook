47e8763734e24dc48517fa0082723f5815087096
Sebastian Bonilla <sbonilla623@gmail.com>
2025-05-08 21:26:42 -0400

    .env fix

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -4,14 +4,15 @@ const cors = require('cors');
 const corsOptions = {
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps, curl requests)
+    // Set CORS_ALLOWED_ORIGINS env variable with comma-separated URLs (e.g. 'http://localhost:3000,https://quickbookai.onrender.com')
     const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS 
-  ? process.env.CORS_ALLOWED_ORIGINS.split(',') 
-  : ['http://localhost:3000'];
-
+      ? process.env.CORS_ALLOWED_ORIGINS.split(',') 
+      : ['http://localhost:3000'];
     
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
+      console.log(`CORS blocked origin: ${origin}`);
       callback(new Error('Not allowed by CORS'));
     }
   },
