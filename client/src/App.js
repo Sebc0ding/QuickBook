5c2bd16994283f71b5f219513807139d5d1c7cc6
Sebastian Bonilla <sbonilla623@gmail.com>
2025-05-14 15:40:36 -0400

    graphql update3

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
+  link: authLink.concat(uploadLink), 
   cache: new InMemoryCache(),
 });
 
